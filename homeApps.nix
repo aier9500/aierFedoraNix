d45eb86b00617fcecde8e53183145796fdba707a
aier9500 <aierjjwu8@gmail.com>
2024-11-13 15:35:15 -0500

    New

---
+++ homeApps.nix
@@ -0,0 +1,58 @@
+{ pkgs, ... }: 
+
+{
+
+  home.packages = (with pkgs; [    # User Apps
+
+    audacity
+    # blender # temporarily disabled
+    chromium
+    darktable
+    dconf2nix
+    eyedropper
+    ferdium
+    gnome-boxes
+    gnome-console
+    gnome-solanum
+    hieroglyphic
+    libreoffice
+    osu-lazer
+    steam
+    tetrio-desktop
+    vesktop
+    vial
+    wordbook
+    # Installed through Flatpak: Zen Browser, Flatseal, GDM-Settings
+
+  ]) ++ (with pkgs.gnomeExtensions; [    # Gnome Extensions
+
+    appindicator
+    arcmenu
+    # auto-move-windows                           # Incompatible with dynamic workspaces
+    blur-my-shell
+    clipboard-indicator
+    dash-to-dock
+    # disable-unredirect-fullscreen-windows       # Not needed for PaperWM
+    # hide-top-bar                                # Incompatible with PaperWM
+    just-perfection
+    launch-new-instance
+    # paperwm
+    quick-settings-audio-panel
+    # rounded-window-corners-reborn               # Incompatible with PaperWM
+    # unite
+    user-themes
+    windownavigator
+    window-title-is-back                          # Not needed for unite
+  ]);
+  
+  services = {
+
+    blanket.enable = true;
+  };
+
+  programs = {
+
+    chromium.enable = true; 
+    texlive.enable = true; 
+  };
+}
---
+++ homeFiles.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, input, ... }:
+
+{
+  home.file = {
+    # fonts
+    ".local/share/fonts/IbmPlex".source = "${pkgs.ibm-plex}/share/fonts/opentype";
+    ".local/share/fonts/NotoCjkSerif".source = "${pkgs.noto-fonts-cjk-serif}/share/fonts/opentype/noto-cjk";
+    ".local/share/fonts/NotoCjkSans".source = "${pkgs.noto-fonts-cjk-sans}/share/fonts/opentype/noto-cjk";
+    # .icons
+    ".local/share/icons/LatteLightCursors".source = "${pkgs.catppuccin-cursors.latteLight}/share/icons/catppuccin-latte-light-cursors";
+    ".local/share/icons/WhitesurCursors".source = "${pkgs.whitesur-cursors}/share/icons/WhiteSur-cursors";
+    # .themes
+    ".local/share/themes/MarbleBlueLight".source = "${pkgs.marble-shell-theme}/share/themes/Marble-blue-light";
+    ".local/share/themes/MarbleBlueDark".source = "${pkgs.marble-shell-theme}/share/themes/Marble-blue-dark";
+    ".local/share/themes/MarblePinkLight".source = "${pkgs.marble-shell-theme}/share/themes/Marble-pink-light";
+    ".local/share/themes/MarblePinkDark".source = "${pkgs.marble-shell-theme}/share/themes/Marble-pink-dark";
+    ".local/share/themes/MarblePurpleLight".source = "${pkgs.marble-shell-theme}/share/themes/Marble-purple-light";
+    ".local/share/themes/MarblePurpleDark".source = "${pkgs.marble-shell-theme}/share/themes/Marble-purple-dark";
+    ".local/share/themes/AdwGtk3".source = "${pkgs.adw-gtk3}/share/themes/adw-gtk3";
+    # MyBash
+    "MyBash/homesw.sh"= {
+      text = 
+        "
+          cd ~/.dotfiles
+          home-manager switch --flake .#default
+          echo \"
+          ----------------------------------------
+          ---------- homesw.sh Finished ----------
+          ----- You can close this window now ----
+          ----------------------------------------
+          \"
+        ";
+      executable = true;
+    };
+    "MyBash/sysw.sh" = {
+      text =
+        "
+          cd ~/.dotfiles
+          sudo nixos-rebuild switch --flake .#default
+          echo \"
+          ----------------------------------------
+          ----------- sysw.sh Finished -----------
+          ----- You can close this window now ----
+          ----------------------------------------
+          \"
+        ";
+      executable = true;
+    };
+    
+  };
+}
