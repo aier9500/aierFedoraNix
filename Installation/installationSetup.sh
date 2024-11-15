7e0744a15fae5806a3959469cefd922539a472a9
aier9500 <aierjjwu8@gmail.com>
2024-11-14 23:15:20 -0400

    Nix = poo

--- Installation/installationSetup.sh
+++ Installation/installationSetup.sh
@@ -1,4 +1,5 @@
 # Prerequisite: Nix
+# The git repository is cloned to ~/.dotfiles/aierFedoraNix
 
 
 # Install Home-Manager
@@ -7,9 +8,9 @@ nix-channel --update
 nix-shell '<home-manager>' -A install
 
 # Using custom .bashrc
-cd ~/.dotfiles/aierFedoraNix
+cd ~/.dotfiles/aierFedoraNix/Installation
 mv ~/.bashrc ~/.bashrcOriginal
-cp ./.bashrc ~/.bashrc
+cp ~/.dotfiles/aierFedoraNix/Installation/.bashrc ~/.bashrc
 
 # Building Home-Manager configs
 home-manager switch --flake .#default --extra-experimental-features "nix-command flakes"
