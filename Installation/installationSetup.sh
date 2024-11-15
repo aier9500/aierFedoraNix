106a92dab8ff5deb209e3f7b7d112e405f80654e
aier9500 <aierjjwu8@gmail.com>
2024-11-14 22:53:04 -0400

    Fedora Arc

--- Installation/installationSetup.sh
+++ Installation/installationSetup.sh
@@ -1,5 +1,11 @@
+# Prerequisite: Nix
+
+# Install Home-Manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
+
+# 
+cd ~/.dotfiles/aierFedoraNix
 mv ~/.bashrc ~/.bashrcOriginal
 cp ./.bashrc ~/.bashrc
