0f19b86880d17d1016bc5456b0bb7989c85a39b7
aier9500 <aierjjwu8@gmail.com>
2024-11-15 21:57:21 -0400

    + VSCode

--- Installation/installDnf.sh
+++ Installation/installDnf.sh
@@ -10,3 +10,8 @@ sudo dnf install gnome-tweaks -y
 sudo dnf install gparted -y
 sudo dnf install obs-studio -y
 sudo dnf install steam -y
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf check-update -y
+sudo dnf install code -y
