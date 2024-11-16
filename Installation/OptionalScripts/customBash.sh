ef75f1cbf495be6c517449c7b714d48d3cd570e5
aier9500 <aierjjwu8@gmail.com>
2024-11-15 22:43:06 -0400

    CustomBash folder

---
+++ Installation/OptionalScripts/customBash.sh
@@ -0,0 +1,7 @@
+# Prerequisites: 
+# The git repository is cloned to ~/.dotfiles/aierFedoraNix
+
+# Using custom .bashrc
+cd ~/.dotfiles/aierFedoraNix/Installation/OptionalScripts
+mv ~/.bashrc ~/.bashrcOriginal
+cp ~/.dotfiles/aierFedoraNix/Installation/OptionalScripts/.bashrc ~/.bashrc
