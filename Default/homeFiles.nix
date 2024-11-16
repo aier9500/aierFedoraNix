1df14f25d7bf1450590ba92405ca7d2b5d83e33c
aier9500 <aierjjwu8@gmail.com>
2024-11-16 15:15:15 -0400

    + fonts to .fonts in addition to .local/share/fonts

--- Default/homeFiles.nix
+++ Default/homeFiles.nix
@@ -6,6 +6,9 @@
     ".local/share/fonts/IbmPlex".source = "${pkgs.ibm-plex}/share/fonts/opentype";
     ".local/share/fonts/NotoCjkSerif".source = "${pkgs.noto-fonts-cjk-serif}/share/fonts/opentype/noto-cjk";
     ".local/share/fonts/NotoCjkSans".source = "${pkgs.noto-fonts-cjk-sans}/share/fonts/opentype/noto-cjk";
+    ".fonts/IbmPlex".source = "${pkgs.ibm-plex}/share/fonts/opentype";
+    ".fonts/NotoCjkSerif".source = "${pkgs.noto-fonts-cjk-serif}/share/fonts/opentype/noto-cjk";
+    ".fonts/NotoCjkSans".source = "${pkgs.noto-fonts-cjk-sans}/share/fonts/opentype/noto-cjk";
     # .icons
     ".local/share/icons/catppuccin-latte-light-cursors".source = "${pkgs.catppuccin-cursors.latteLight}/share/icons/catppuccin-latte-light-cursors";
     ".local/share/icons/WhiteSur-cursors".source = "${pkgs.whitesur-cursors}/share/icons/WhiteSur-cursors";
