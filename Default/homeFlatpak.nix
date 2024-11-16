5a30efc09a39085a1d554e8f726e77a3a0ec74d4
aier9500 <aierjjwu8@gmail.com>
2024-11-16 18:08:16 -0400

    Enabling flatpak service

--- Default/homeFlatpak.nix
+++ Default/homeFlatpak.nix
@@ -1,27 +1,33 @@
 { config, ... }: 
 
 {
+
+  services.flatpak.enable = true; 
+  
   # Auto Update
   services.flatpak.update.onActivation = true;
 
   # Declare Flatpaks to install here
   services.flatpak.packages = [
-    "com.github.finefindus.eyedropper -y"
-    "com.github.tchx84.Flatseal -y"
-    "com.mattjakeman.ExtensionManager -y"
-    "com.protonvpn.www -y"
-    "com.rafaelmardojai.Blanket -y"
-    "dev.mufeed.Wordbook -y"
-    "fr.handbrake.ghb -y"
-    "io.github.finefindus.Hieroglyphic -y"
-    "io.github.zen_browser.zen -y"
-    "net.nokyan.Resources -y"
-    "org.ferdium.Ferdium -y"
-    "org.gnome.gitlab.YaLTeR.VideoTrimmer -y"
-    "org.gnome.Solanum -y"
-    "org.gtk.Gtk3theme.adw-gtk3 -y"
-    "org.localsend.localsend_app -y"
-    "sh.ppy.osu -y"
+    "com.github.finefindus.eyedropper"
+    "com.github.tchx84.Flatseal"
+    "com.mattjakeman.ExtensionManager"
+    "com.protonvpn.www"
+    "com.rafaelmardojai.Blanket"
+    "dev.mufeed.Wordbook "
+    "fr.handbrake.ghb"
+    "io.github.finefindus.Hieroglyphic"
+    "io.github.zen_browser.zen"
+    "net.nokyan.Resources"
+    "org.ferdium.Ferdium"
+    "org.gnome.gitlab.YaLTeR.VideoTrimmer"
+    "org.gnome.Solanum"
+    "org.gtk.Gtk3theme.adw-gtk3"
+    "org.localsend.localsend_app"
+    "sh.ppy.osu"
+
+    # Testing
+    "dev.geopjr.Calligraphy"
   ];
 
   # Flatpak override configs
