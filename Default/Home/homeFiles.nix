0d4d57fee7a47447fbfb1f2ff51ec8ddcdd66574
aier9500 <aierjjwu8@gmail.com>
2024-11-17 19:04:57 -0400

    Declaring kanata dotfiles

--- Default/Home/homeFiles.nix
+++ Default/Home/homeFiles.nix
@@ -33,6 +33,64 @@
     ".themes/adw-gtk3-dark".source = "${pkgs.adw-gtk3}/share/themes/adw-gtk3-dark";
 
     # dotfiles
-    
+    ".config/kanata/kanata.kbd".text = "
+(defsrc
+  esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12
+  grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+  tab   q    w    e    r    t    y    u    i    o    p    [    ]    
+  caps   a    s    d    f    g    h    j    k    l    ;    '    ret
+  lsft     z    x    c    v    b    n    m    ,    .    /    rsft
+  lctl lmet lalt           spc            ralt rmet rctl
+)
+
+(deflayer default
+  esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12
+  grv  1    2    3    4    5    6    7    8    9    0    -    =    @bcp
+  tab   q    w    e    r    t    y    u    i    o    p    [    ]    
+  bspc   a    s    d    f    g    h    j    k    l    ;    '    ret
+  lsft     z    x    c    v    b    n    m    ,    .    /    rsft
+  lctl lmet @anv           spc            ralt rmet rctl
+)  
+
+(deflayer navigation
+  XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX
+  XX   @lqw @lcm XX   XX   XX   XX   XX   XX   XX   XX   XX   XX   XX
+  XX    XX   XX   XX   XX   XX   XX   @clt up   @crt XX   XX   XX   
+  XX     XX   XX   XX   XX   XX   XX   lft  down rght XX   XX   XX
+  lsft     lalt XX   XX   XX   XX   XX   XX   home end  XX   XX
+  lctl lmet XX             XX             XX   XX   XX 
+)
+
+(deflayer colemak-dh
+  esc  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  f11  f12
+  grv  1    2    3    4    5    6    7    8    9    0    -    =    @bcp
+  tab   q    w    f    p    b    j    l    u    y    ;    [    ]    
+  bspc   a    r    s    t    g    m    n    e    i    o    '    ret
+  lsft     x    c    d    v    z    k    h    ,    .    /    rsft
+  lctl lmet @anv           spc            ralt rmet rctl
+)  
+
+(defalias
+  bcp ( tap-hold     200  200  bspc caps )                             ;; tap: bspc | hold: caps
+  anv ( tap-dance    200 ( lalt (layer-while-held navigation) ) )      ;; tap1: lalt | tap2&hold: navigation layer
+  clt ( macro C-(lft  10) )                                            ;; ctl + lft                      
+  crt ( macro C-(rght 10) )                                            ;; ctl + rght
+  lqw ( layer-switch default )                                         ;; Switch to QWERTY layer
+  lcm ( layer-switch colemak-dh)                                       ;; Switch to Colemak-DH layer
+)
+    ";
+    ".config/systemd/user/kanata.service".text = "
+[Unit]
+Description=Kanata keyboard remapper
+Documentation=https://github.com/jtroo/kanata
+
+[Service]
+Type=simple
+ExecStart=/home/%u/.local/share/bin/kanata
+Restart=no
+
+[Install]
+WantedBy=default.target
+    ";
   };
 }
