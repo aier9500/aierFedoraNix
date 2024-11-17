af027a918cdaf78ab484edf87e7a7087fda4ae86
aier9500 <aierjjwu8@gmail.com>
2024-11-17 16:53:08 -0400

    Cleaning up dconf settings

--- Default/Home/HomeDconf/gnomeDesktopWMPreferences.nix
+++ Default/Home/HomeDconf/gnomeDesktopWMPreferences.nix
@@ -7,9 +7,6 @@ with lib.hm.gvariant;
   dconf.settings = {
     "org/gnome/desktop/wm/preferences" = {
       action-middle-click-titlebar = "minimize";
-      button-layout = "appmenu:close";
-      focus-mode = "click";
-      num-workspaces = 3;
     };
 
   };
--- Default/Home/homeDconf.nix
+++ Default/Home/homeDconf.nix
@@ -12,11 +12,10 @@ with lib.hm.gvariant;
     ./HomeDconf/gnomeExtensionsBlurMyShell.nix
     ./HomeDconf/gnomeExtensionsClipboardManager.nix
     ./HomeDconf/gnomeExtensionsDashToDock.nix
-    ./HomeDconf/gnomeExtensionsHidetopbar.nix
+    # ./HomeDconf/gnomeExtensionsHidetopbar.nix
     ./HomeDconf/gnomeExtensionsJustPerfection.nix
     ./HomeDconf/gnomeExtensionsQuickSettingsAudioPanel.nix
-    ./HomeDconf/gnomeExtensionsRoundedWindowCornersReborn.nix
-    ./HomeDconf/gnomeExtensionsUnite.nix
+    # ./HomeDconf/gnomeExtensionsUnite.nix
     ./HomeDconf/gnomeExtensionsUserTheme.nix
     ./HomeDconf/gnomeMediaKeys.nix
     ./HomeDconf/gnomeMouse.nix
