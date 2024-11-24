d4634f987848e2a0cd0a7e614e741b8e22722a3e
aier9500 <aierjjwu8@gmail.com>
2024-11-24 14:25:17 -0400

    Moved Flatpak apps

--- Default/Home/homeApps.nix
+++ Default/Home/homeApps.nix
@@ -1,26 +1,62 @@
-{ pkgs, ... }: 
+{ pkgs, configs, ... }: 
 
 {
 
+  # ---- Not from Nixpkgs, DNF, nor Flatpak ----
+  # scrcpy
+  # tetrio-desktop
+  # vesktop
+  # vial
+
+
+  # Declare Flatpaks to install here
+  services.flatpak.packages = [
+    "com.github.finefindus.eyedropper"
+    "com.github.tchx84.Flatseal"
+    "com.mattjakeman.ExtensionManager"
+    "com.protonvpn.www"
+    "com.rafaelmardojai.Blanket"
+    "de.haeckerfelix.Fragments"
+    "dev.geopjr.Calligraphy"
+    "dev.mufeed.Wordbook "
+    "fr.handbrake.ghb"
+    "io.github.finefindus.Hieroglyphic"
+    "io.github.flattool.Warehouse"
+    "io.github.zen_browser.zen"
+    "net.nokyan.Resources"
+    "org.ferdium.Ferdium"
+    "org.gnome.gitlab.YaLTeR.VideoTrimmer"
+    "org.gnome.Solanum"
+    "org.gtk.Gtk3theme.adw-gtk3"
+    "org.localsend.localsend_app"
+    "org.signal.Signal"
+    "org.texstudio.TeXstudio"
+    "sh.ppy.osu"
+  ];
+
   # User Apps
   home.packages = (with pkgs; [    
+    
+    (import ./HomeApps/HomeAppsVesktop.nix { inherit pkgs; })
 
     dconf2nix
+    eza
+    fastfetch
+    fd
     ffmpeg-full
+    fzf
+    git
     gnome-themes-extra # Theming dependency
     gtk-engine-murrine # Theming dependency
+    java
     kanata
     ntfs3g
     python3
+    ripgrep
     sassc # Theming dependency
     texliveFull
     usbutils # lsusb etc.
-
-    # ---- Not from Nixpkgs, DNF, nor Flatpak ----
-    # scrcpy
-    # tetrio-desktop
-    # vesktop
-    # vial
+    zoxide
 
   # Gnome Extensions
   ]) ++ (with pkgs.gnomeExtensions; [
@@ -39,27 +75,4 @@
     windownavigator
   ]);
   
-  services = {
-
-  };
-
-  programs = {
-
-    # Non-Graphical Programs
-    eza = {
-      enable = true;
-      enableBashIntegration = true;
-    };
-    fastfetch.enable = true;
-    fd.enable = true; 
-    fzf.enable = true;
-    git.enable = true;
-    java.enable = true; 
-    ripgrep.enable = true;
-    zoxide = {
-      enable = true;
-      enableBashIntegration = true;
-    };
-
-  };
 }
--- Default/Home/homeFlatpak.nix
+++ Default/Home/homeFlatpak.nix
@@ -7,30 +7,6 @@
   # Auto Update On
   services.flatpak.update.onActivation = true;
 
-  # Declare Flatpaks to install here
-  services.flatpak.packages = [
-    "com.github.finefindus.eyedropper"
-    "com.github.tchx84.Flatseal"
-    "com.mattjakeman.ExtensionManager"
-    "com.protonvpn.www"
-    "com.rafaelmardojai.Blanket"
-    "de.haeckerfelix.Fragments"
-    "dev.geopjr.Calligraphy"
-    "dev.mufeed.Wordbook "
-    "fr.handbrake.ghb"
-    "io.github.finefindus.Hieroglyphic"
-    "io.github.flattool.Warehouse"
-    "io.github.zen_browser.zen"
-    "net.nokyan.Resources"
-    "org.ferdium.Ferdium"
-    "org.gnome.gitlab.YaLTeR.VideoTrimmer"
-    "org.gnome.Solanum"
-    "org.gtk.Gtk3theme.adw-gtk3"
-    "org.localsend.localsend_app"
-    "org.texstudio.TeXstudio"
-    "sh.ppy.osu"
-  ];
-
   # Flatpak override configs
   services.flatpak.overrides = {
 
