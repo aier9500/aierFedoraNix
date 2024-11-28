ca89abb6a4a23ff5d7cd565b998a8a7b5718309b
aier9500 <aierjjwu8@gmail.com>
2024-11-28 08:24:07 -0400

    + scrcpy

--- Installation/installDnf.sh
+++ Installation/installDnf.sh
@@ -5,5 +5,8 @@ sudo dnf install gparted -y
 # VScode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf check-update -y
+sudo dnf check-update -y
 sudo dnf install code -y
+
+# Screen Copy
+sudo dnf copr enable zeno/scrcpy -y && sudo dnf install scrcpy -y
