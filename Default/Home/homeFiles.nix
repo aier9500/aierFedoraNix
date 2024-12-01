ca34892fa16477b05e4f0247875e61509260f11b
aier9500 <aierjjwu8@gmail.com>
2024-12-01 17:15:00 -0400

    Mass restructure

--- Default/Home/homeFiles.nix
+++ Default/Home/homeFiles.nix
@@ -4,33 +4,22 @@
   home.file = {
 
     # fonts
-    ".local/share/fonts/IbmPlex".source = "${pkgs.ibm-plex}/share/fonts/opentype";
-    ".local/share/fonts/NotoCjkSerif".source = "${pkgs.noto-fonts-cjk-serif}/share/fonts/opentype/noto-cjk";
-    ".local/share/fonts/NotoCjkSans".source = "${pkgs.noto-fonts-cjk-sans}/share/fonts/opentype/noto-cjk"; 
-    # .fonts
-    ".fonts/IbmPlex".source = "${pkgs.ibm-plex}/share/fonts/opentype";
-    ".fonts/NotoCjkSerif".source = "${pkgs.noto-fonts-cjk-serif}/share/fonts/opentype/noto-cjk";
-    ".fonts/NotoCjkSans".source = "${pkgs.noto-fonts-cjk-sans}/share/fonts/opentype/noto-cjk";
+    ".local/share/fonts/ibm-plex".source = "${pkgs.ibm-plex}/share/fonts/opentype";
+    ".local/share/fonts/noto-fonts-cjk-serif".source = "${pkgs.noto-fonts-cjk-serif}/share/fonts/opentype/noto-cjk";
+    ".local/share/fonts/noto-fonts-cjk-sans".source = "${pkgs.noto-fonts-cjk-sans}/share/fonts/opentype/noto-cjk"; 
 
     # icons
+    ".local/share/icons/Bibata-Modern-Ice".source = "${pkgs.bibata-cursors}/share/icons/Bibata-Modern-Ice";
     # ".local/share/icons/catppuccin-latte-light-cursors".source = "${pkgs.catppuccin-cursors.latteLight}/share/icons/catppuccin-latte-light-cursors"; # Temporarily disabled
     # ".local/share/icons/WhiteSur-cursors".source = "${pkgs.whitesur-cursors}/share/icons/WhiteSur-cursors";
-    # .icons
-    # ".icons/catppuccin-latte-light-cursors".source = "${pkgs.catppuccin-cursors.latteLight}/share/icons/catppuccin-latte-light-cursors"; # Temporarily disabled
-    # ".icons/WhiteSur-cursors".source = "${pkgs.whitesur-cursors}/share/icons/WhiteSur-cursors";
 
     # themes
     ".local/share/themes/Marble-blue-light".source = "${pkgs.marble-shell-theme}/share/themes/Marble-blue-light";
     ".local/share/themes/Marble-blue-dark".source = "${pkgs.marble-shell-theme}/share/themes/Marble-blue-dark";
-    ".local/share/themes/Marble-pink-light".source = "${pkgs.marble-shell-theme}/share/themes/Marble-pink-light";
-    ".local/share/themes/Marble-pink-dark".source = "${pkgs.marble-shell-theme}/share/themes/Marble-pink-dark";
     ".local/share/themes/Marble-purple-light".source = "${pkgs.marble-shell-theme}/share/themes/Marble-purple-light";
     ".local/share/themes/Marble-purple-dark".source = "${pkgs.marble-shell-theme}/share/themes/Marble-purple-dark";
     ".local/share/themes/adw-gtk3".source = "${pkgs.adw-gtk3}/share/themes/adw-gtk3";
     ".local/share/themes/adw-gtk3-dark".source = "${pkgs.adw-gtk3}/share/themes/adw-gtk3-dark";
-    # .themes
-    ".themes/adw-gtk3".source = "${pkgs.adw-gtk3}/share/themes/adw-gtk3";
-    ".themes/adw-gtk3-dark".source = "${pkgs.adw-gtk3}/share/themes/adw-gtk3-dark";
 
     # dotfiles
 
