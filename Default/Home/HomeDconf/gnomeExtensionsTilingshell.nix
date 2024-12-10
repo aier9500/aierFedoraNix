c32eee540d03ad094e65d3479371ea3914ebb93a
aier9500 <aierjjwu8@gmail.com>
2024-12-10 19:27:59 -0400

    Configuring Tilingshell

--- Default/Home/HomeDconf/gnomeExtensionsTilingshell.nix
+++ Default/Home/HomeDconf/gnomeExtensionsTilingshell.nix
@@ -6,14 +6,13 @@ with lib.hm.gvariant;
 {
   dconf.settings = {
     "org/gnome/shell/extensions/tilingshell" = {
-      enable-move-keybindings = false;
+      enable-move-keybindings = true; # False
       enable-snap-assist = false; # Default: true
+      enable-window-border = true;
       inner-gaps = mkUint32 4;
       outer-gaps = mkUint32 4;
-      overridden-settings = ''
-        {"org.gnome.mutter":{"edge-tiling":"true"}}
-      '';
       quarter-tiling-threshold = mkUint32 15;
+      show-indicator = true; 
       tile-preview-animation-time = mkUint32 180;
       top-edge-maximize = true; 
     };
