e665ac12b0de5bfa5263f1f04c048e9133b0f711
aier9500 <aierjjwu8@gmail.com>
2024-12-15 19:58:58 -0400

    Changing userjs

--- Installation/OptionalScripts/user.js
+++ Installation/OptionalScripts/user.js
@@ -1,32 +1,49 @@
-/****************************************************************************************
- * OPTION: NATURAL SMOOTH SCROLLING V3 [MODIFIED]                                      *
-****************************************************************************************/
-// credit: https://github.com/AveYo/fox/blob/cf56d1194f4e5958169f9cf335cd175daa48d349/Natural%20Smooth%20Scrolling%20for%20user.js
-// recommended for 120hz+ displays
-// largely matches Chrome flags: Windows Scrolling Personality and Smooth Scrolling
-user_pref("apz.overscroll.enabled", false); // DEFAULT NON-LINUX TRUE
+// Smooth Scrolling
 user_pref("general.smoothScroll", true); // DEFAULT
 user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
 user_pref("general.smoothScroll.msdPhysics.enabled", true);
-user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
-user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 650);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 125);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 250);
 user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 25);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", "2");
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", "2.0");
 user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
-user_pref("general.smoothScroll.currentVelocityWeighting", "1");
-user_pref("general.smoothScroll.stopDecelerationWeighting", "1");
+user_pref("general.smoothScroll.currentVelocityWeighting", "1.0");
+user_pref("general.smoothScroll.stopDecelerationWeighting", "1.0");
 
 
-// Custom
+// Mousewheel
+user_pref("mousewheel.system_scroll_override.horizontal.factor", 200);
+user_pref("mousewheel.system_scroll_override.vertical.factor", 200);
+user_pref("mousewheel.system_scroll_override_on_root_content.enabled", true);
+user_pref("mousewheel.system_scroll_override.enabled", true);
+// Mousewheel & Touchpad
+user_pref("mousewheel.default.delta_multiplier_x", 100);
+user_pref("mousewheel.default.delta_multiplier_y", 100);
+user_pref("mousewheel.default.delta_multiplier_z", 100);
+
+
+// Other Scrolling
+user_pref("apz.overscroll.enabled", false); // DEFAULT NON-LINUX TRUE
 user_pref("general.autoScroll", true);
-user_pref("apz.fling_friction", "0.0025");
+user_pref("apz.fling_friction", "0.002");
+
 user_pref("apz.gtk.pangesture.delta_mode", 2);
-user_pref("apz.gtk.pangesture.pixel_delta_mode_multiplier", "5.0");
-user_pref("mousewheel.default.delta_multiplier_x", 300);
-user_pref("mousewheel.default.delta_multiplier_y", 300);
-user_pref("mousewheel.default.delta_multiplier_z", 300);
+user_pref("apz.gtk.pangesture.pixel_delta_mode_multiplier", "10.0");
+
+
 
 // Zen
 user_pref("zen.glance.activation-method", "alt");
-user_pref("zen.workspaces.show-icon-strip", false);
 user_pref("zen.sidebar.close-on-blur", false);
+user_pref("zen.splitView.change-on-hover", true); 
+user_pref("zen.view.compact.hide-toolbar", true); 
+user_pref("zen.workspaces.show-icon-strip", false);
+
+// Super Pins
+user_pref("uc.essentials.box-like-corners", false);
+user_pref("uc.essentials.color-scheme", "");
+user_pref("uc.essentials.gap", "");
+user_pref("uc.essentials.width", "");
+user_pref("uc.pins.bg", true);
+user_pref("uc.pins.legacy-layout", true);
+user_pref("uc.superpins.border", "essentials");
