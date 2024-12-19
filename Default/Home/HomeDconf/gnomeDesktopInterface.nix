46d1fddf99197d21a66d1a6ccb8b7e02e64c8bc6
aier9500 <aier9500>
2024-12-19 16:44:15 -0500

    Changing default fonts

--- Default/Home/HomeDconf/gnomeDesktopInterface.nix
+++ Default/Home/HomeDconf/gnomeDesktopInterface.nix
@@ -9,14 +9,14 @@ with lib.hm.gvariant;
       clock-format = "24h"; 
       color-scheme = "default"; # Prefer dark or light
       cursor-theme = "Bibata-Modern-Ice"; # Adwaita
-      document-font-name = "Noto Serif CJK TC 12.5 @wght=400"; # Serif/Document font # Default pt 11
+      document-font-name = "Noto Serif CJK TC 11 @wght=400"; # Serif/Document font # Default pt 11, alt 12.5
       enable-hot-corners = false;
       font-antialiasing = "rgba";
       font-hinting = "full";
-      font-name = "IBM Plex Sans 12.5"; # Sans/Interface/Backup font # Default pt 11
+      font-name = "IBM Plex Sans 11"; # Sans/Interface/Backup font # Default pt 11, alt 12.5
       gtk-enable-primary-paste = false;
       gtk-theme = "adw-gtk3";
-      monospace-font-name = "IBM Plex Mono 12"; # Mono font # default 10
+      monospace-font-name = "IBM Plex Mono 10"; # Mono font # default 10, alt 12
       show-battery-percentage = true;
       text-scaling-factor = 1; 
     };
