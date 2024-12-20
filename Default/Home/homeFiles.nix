95051e5e59334ee315da7d5549ae9eedc5860fbb
aier9500 <aier9500>
2024-12-20 15:57:16 -0500

    Changing animations

--- Default/Home/homeFiles.nix
+++ Default/Home/homeFiles.nix
@@ -40,7 +40,9 @@
       [burn-my-windows-profile]
       fire-enable-effect=false
       glide-enable-effect=true
+      glide-animation-time=200
       glide-scale=0.10000000000000001
+      glide-shift=-1.0    
     '';
 
     # Kanata
