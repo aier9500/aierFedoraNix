e0b23d205062052a2f2f16e412b028b1ebb68025
aier9500 <aier9500>
2024-12-20 15:57:21 -0500

    Arc

--- Installation/OptionalScripts/user.js
+++ Installation/OptionalScripts/user.js
@@ -1,40 +1,18 @@
-// Smooth Scrolling
-user_pref("general.smoothScroll", true); // DEFAULT
-user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
-user_pref("general.smoothScroll.msdPhysics.enabled", true);
-user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 125);
-user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 250);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 25);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", "2.0");
-user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
-user_pref("general.smoothScroll.currentVelocityWeighting", "1.0");
-user_pref("general.smoothScroll.stopDecelerationWeighting", "1.0");
-
-
-// Mousewheel
-user_pref("mousewheel.system_scroll_override.horizontal.factor", 200);
-user_pref("mousewheel.system_scroll_override.vertical.factor", 200);
-user_pref("mousewheel.system_scroll_override_on_root_content.enabled", true);
-user_pref("mousewheel.system_scroll_override.enabled", true);
-// Mousewheel & Touchpad
-user_pref("mousewheel.default.delta_multiplier_x", 100);
-user_pref("mousewheel.default.delta_multiplier_y", 100);
-user_pref("mousewheel.default.delta_multiplier_z", 100);
-
-
-// Other Scrolling
-user_pref("apz.overscroll.enabled", false); // DEFAULT NON-LINUX TRUE
-user_pref("general.autoScroll", true);
-user_pref("apz.fling_friction", "0.002");
-
-user_pref("apz.gtk.pangesture.delta_mode", 2);
-user_pref("apz.gtk.pangesture.pixel_delta_mode_multiplier", "10.0");
-
-
-
 // Zen
 user_pref("zen.glance.activation-method", "alt");
 user_pref("zen.sidebar.close-on-blur", false);
 user_pref("zen.splitView.change-on-hover", true); 
 user_pref("zen.view.compact.hide-toolbar", true); 
 user_pref("zen.workspaces.show-icon-strip", false);
+
+// Scrolling
+user_pref("apz.fling_friction", "0.003");
+user_pref("apz.gtk.pangesture.delta_mode", 2);
+user_pref("apz.gtk.pangesture.pixel_delta_mode_multiplier", "20");
+
+user_pref("apz.overscroll.enabled", false); // DEFAULT NON-LINUX TRUE
+user_pref("general.autoScroll", true);
+
+user_pref("mousewheel.default.delta_multiplier_x", 100);
+user_pref("mousewheel.default.delta_multiplier_y", 100);
+user_pref("mousewheel.default.delta_multiplier_z", 100);
