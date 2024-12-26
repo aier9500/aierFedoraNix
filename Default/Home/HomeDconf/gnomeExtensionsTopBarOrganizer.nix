3f405d9131b7ffd2a6194d5f04d366e841b54b53
aier9500 <aier9500>
2024-12-26 13:14:04 -0400

    Organizing top bar

--- Default/Home/HomeDconf/gnomeExtensionsTopBarOrganizer.nix
+++ Default/Home/HomeDconf/gnomeExtensionsTopBarOrganizer.nix
@@ -6,8 +6,9 @@ with lib.hm.gvariant;
 {
   dconf.settings = {
     "org/gnome/shell/extensions/top-bar-organizer" = {
-      center-box-order = [ "uniteAppMenu" "uniteDesktopLabel" ];
       left-box-order = [ "activities" "system-monitor@gnome-shell-extensions.gcampax.github.com" ];
+      center-box-order = [ "uniteAppMenu" "uniteDesktopLabel" ];
+      right-box-order = [ "screenRecording" "screenSharing" "clipboardIndicator" "dwellClick" "a11y" "keyboard" "dateMenu" "quickSettings" ];
     };
 
   };
