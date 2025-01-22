98bd008330608ded8fdc3dbe5bc6bcc78617f1fa
aier9500 <aier9500>
2025-01-22 19:03:31 -0400

    Focus mode 'mouse'

--- Default/Home/HomeDconf/gnomeDesktopWMPreferences.nix
+++ Default/Home/HomeDconf/gnomeDesktopWMPreferences.nix
@@ -7,7 +7,7 @@ with lib.hm.gvariant;
   dconf.settings = {
     "org/gnome/desktop/wm/preferences" = {
       action-middle-click-titlebar = "minimize";
-      focus-mode = "sloppy"; 
+      focus-mode = "mouse"; 
     };
 
   };
