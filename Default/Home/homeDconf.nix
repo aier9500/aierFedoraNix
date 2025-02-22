7f03c3bebe6ea0aff45deac292517aaed2eadbdd
aier9500 <aierjjwu8@gmail.com>
2025-02-22 12:39:32 -0400

    Simplifying naming scheme

--- Default/Home/homeDconf.nix
+++ Default/Home/homeDconf.nix
@@ -6,17 +6,16 @@ with lib.hm.gvariant;
   
   imports = [
     ./HomeDconf/desktop.nix
-    ./HomeDconf/gnomeExtensionsAppindicator.nix
-    # ./HomeDconf/gnomeExtensionsArcmenu.nix
-    ./HomeDconf/gnomeExtensionsCaffeine.nix
-    # ./HomeDconf/gnomeExtensionsClipboardManager.nix
-    ./HomeDconf/gnomeExtensionsFocusChanger.nix
-    ./HomeDconf/gnomeExtensionsJustPerfection.nix
-    ./HomeDconf/gnomeExtensionsPano.nix
-    ./HomeDconf/gnomeExtensionsQuickSettingsAudioPanel.nix
-    ./HomeDconf/gnomeExtensionsUnite.nix
-    ./HomeDconf/gnomeMediaKeys.nix
+    ./HomeDconf/extensionsAppindicator.nix
+    # ./HomeDconf/extensionsArcmenu.nix
+    ./HomeDconf/extensionsCaffeine.nix
+    # ./HomeDconf/extensionsClipboardManager.nix
+    # ./HomeDconf/extensionsFocusChanger.nix
+    ./HomeDconf/extensionsJustPerfection.nix
+    ./HomeDconf/extensionsPano.nix
+    ./HomeDconf/extensionsUnite.nix
     ./HomeDconf/keybindings.nix
+    ./HomeDconf/mediaKeys.nix
     ./HomeDconf/mutter.nix
     ./HomeDconf/peripherals.nix
   ];
