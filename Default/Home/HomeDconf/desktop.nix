9fe250c1edb6e788a6bde06fc1092a8305de5761
aier9500 <aierjjwu8@gmail.com>
2025-03-05 20:41:12 -0400

    Updated desktop

--- Default/Home/HomeDconf/desktop.nix
+++ Default/Home/HomeDconf/desktop.nix
@@ -14,7 +14,7 @@ with lib.hm.gvariant;
       enable-hot-corners = false; 
       font-antialiasing = "rgba";
       font-hinting = "full";
-      font-name = "IBM Plex Sans 11"; # Sans/Interface/Backup font # Default pt 11, alt 12.5
+      font-name = "IBM Plex Sans 10.5"; # Sans/Interface/Backup font # Default pt 11, alt 12.5
       gtk-enable-primary-paste = false;
       gtk-theme = "adw-gtk3";
       monospace-font-name = "IBM Plex Mono 10"; # Mono font # default 10, alt 12
