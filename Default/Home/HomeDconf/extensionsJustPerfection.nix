fd343c586233eca1a8deebec1e2167b7f15098af
aier9500 <aierjjwu8@gmail.com>
2025-03-07 17:58:49 -0400

    Updating just perfection settings and comments

--- Default/Home/HomeDconf/extensionsJustPerfection.nix
+++ Default/Home/HomeDconf/extensionsJustPerfection.nix
@@ -6,17 +6,17 @@ with lib.hm.gvariant;
 {
   dconf.settings = {
     "org/gnome/shell/extensions/just-perfection" = {
-      animation = 5;
-      dash = false; 
-      notification-banner-position = 1; # 1 for top center, 2 for top right, 5 for bottom right
-      panel = true;
-      panel-in-overview = true;
-      panel-size = 32; 
-      search = false;
-      workspaces-in-app-grid = true; # false for clean app grid
-      window-preview-caption = false;
-      workspace-switcher-should-show = true;
-      workspace-switcher-size = 15;
+      animation = 1; # 1 # 5 is fast, 4 is slow
+      dash = false;
+      notification-banner-position = 1; #1 # 1 for top center, 2 for top right, 5 for bottom right
+      panel = true; # true
+      panel-in-overview = true; # true
+      panel-size = 0; # 0
+      search = false; # true
+      workspaces-in-app-grid = true; # true # false for clean app grid
+      window-preview-caption = false; # true
+      workspace-switcher-should-show = true; # false
+      workspace-switcher-size = 13; # 0
     };
 
   };
