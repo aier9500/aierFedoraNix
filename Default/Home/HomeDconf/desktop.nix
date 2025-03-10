ce5c97a27fd11b6b482bad0aef252d9b6300e68c
aier9500 <aierjjwu8@gmail.com>
2025-03-10 14:05:13 -0400

    Default panel-run-dialog keybinding

--- Default/Home/HomeDconf/desktop.nix
+++ Default/Home/HomeDconf/desktop.nix
@@ -46,7 +46,7 @@ with lib.hm.gvariant;
       move-to-workspace-left = [ "<Shift><Control><Super>left" "<Super>bracketleft" ];
       move-to-workspace-right = [ "<Shift><Control><Super>right" "<Super>bracketright" ];
       move-to-workspace-up = [];
-      panel-run-dialog = [ "<Super>r" ];
+      panel-run-dialog = [ "<Alt>F2" ]; # <Alt>F2
       show-desktop = [ "<Super>d" ];
       switch-applications = [ "<Super>Tab" ];
       switch-applications-backward = [ "<Shift><Super>Tab" ];
