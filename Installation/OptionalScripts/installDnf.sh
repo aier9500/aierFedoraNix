4e115ce1c69672599c08e6e2678f1873b47eea84
aier9500 <aierjjwu8@gmail.com>
2025-03-18 14:24:31 -0400

    commenting out some optional apps. Will be included in wiki in the future.

--- Installation/OptionalScripts/installDnf.sh
+++ Installation/OptionalScripts/installDnf.sh
@@ -3,7 +3,7 @@ sudo dnf install dconf-editor -y
 sudo dnf install gnome-tweaks -y
 sudo dnf install gparted -y
 sudo dnf install timeshift -y
-sudo dnf install waydroid -y
+# sudo dnf install waydroid -y
 
 # VScode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
@@ -12,16 +12,16 @@ sudo dnf check-update -y
 sudo dnf install code -y
 
 # Screen Copy
-sudo dnf copr enable zeno/scrcpy -y && sudo dnf install scrcpy -y
+# sudo dnf copr enable zeno/scrcpy -y && sudo dnf install scrcpy -y
 
 # Hyprland
-sudo dnf install hyprland -y
-sudo dnf install hyprshot -y
-sudo dnf install hyprpaper -y
-sudo dnf install blueman -y 
-sudo dnf install nm-applet -y
-sudo dnf install copyq -y
-sudo dnf install swaync -y
-sudo dnf install wlogout -y
-sudo dnf install pavucontrol -y
-flatpak install com.obsproject.Studio
+# sudo dnf install hyprland -y
+# sudo dnf install hyprshot -y
+# sudo dnf install hyprpaper -y
+# sudo dnf install blueman -y 
+# sudo dnf install nm-applet -y
+# sudo dnf install copyq -y
+# sudo dnf install swaync -y
+# sudo dnf install wlogout -y
+# sudo dnf install pavucontrol -y
+# flatpak install com.obsproject.Studio
