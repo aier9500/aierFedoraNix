6aaf0b7a496896a9a7f8a02a6b2861689281b648
aier9500 <aierjjwu8@gmail.com>
2025-04-01 13:20:28 -0400

    Cleaning up the filestructure

---
+++ Installation/OptionalScripts/Bash/customBash.sh
@@ -0,0 +1,7 @@
+# Prerequisites: 
+# The git repository is cloned to ~/.dotfiles
+
+# Using custom .bashrc
+cd ~/.dotfiles/aierFedoraNix/Installation/OptionalScripts/Bash/
+cp ~/.bashrc ~/.bashrcOriginal
+cat ~/.dotfiles/aierFedoraNix/Installation/OptionalScripts/Bash/.bashrc >> ~/.bashrc
--- Installation/OptionalScripts/customBash.sh
+++
@@ -1,7 +0,0 @@
-# Prerequisites: 
-# The git repository is cloned to ~/.dotfiles
-
-# Using custom .bashrc
-cd ~/.dotfiles/aierFedoraNix/Installation/OptionalScripts
-cp ~/.bashrc ~/.bashrcOriginal
-cat ~/.dotfiles/aierFedoraNix/Installation/OptionalScripts/.bashrc >> ~/.bashrc
