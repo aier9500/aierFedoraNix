fed273a319606b13120e92ff83b2eaf21f16320a
aier9500 <aierjjwu8@gmail.com>
2025-05-17 16:06:14 -0400

    Updating properties for firefox scroll

--- Installation/Dotfiles/user.js
+++ Installation/Dotfiles/user.js
@@ -2,21 +2,19 @@ user_pref("browser.tabs.hoverPreview.enabled", true);
 
 // Zen
 user_pref("zen.glance.activation-method", "alt");
-// user_pref("zen.sidebar.close-on-blur", false); Legacy
 user_pref("zen.splitView.change-on-hover", true);
 user_pref("zen.tabs.vertical.right-side", true);
 user_pref("zen.view.compact.hide-toolbar", true);
 user_pref("zen.view.compact.toolbar-flash-popup", false);
 user_pref("zen.view.show-newtab-button-top", false);
-// user_pref("zen.workspaces.show-icon-strip", false); // Legacy
 user_pref("zen.workspaces.show-workspace-indicator", true);
 
 // Scrolling
-user_pref("apz.fling_friction", "0.002");
+user_pref("apz.fling_friction", "0.005");
 user_pref("apz.gtk.pangesture.delta_mode", 2);
-user_pref("apz.gtk.pangesture.pixel_delta_mode_multiplier", "10"); // 10
+user_pref("apz.gtk.pangesture.pixel_delta_mode_multiplier", "14");
 
-user_pref("apz.overscroll.enabled", false); // DEFAULT NON-LINUX TRUE
+user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX TRUE
 user_pref("general.autoScroll", true);
 
 user_pref("mousewheel.default.delta_multiplier_x", 100);
